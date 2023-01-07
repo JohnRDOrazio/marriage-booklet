165acb10038f5d069b47f4bf5b38dd20358ca46d
codefactor-io <support@codefactor.io>
2023-01-07 01:06:17 +0000

    [CodeFactor] Apply fixes

--- gem_deps.rb
+++ gem_deps.rb
@@ -2,9 +2,9 @@ require 'bundler'
 require 'json'
 
     def add_to_dep(dep,top_level)
-      exists = top_level.key?("#{dep.name}")
+      exists = top_level.key?(dep.name.to_s)
       if !exists
-        top_level.merge!("#{dep.name}" => "#{dep.requirement.requirements.join}")
+        top_level.merge!(dep.name.to_s => dep.requirement.requirements.join.to_s)
       end  
     end
 
@@ -17,6 +17,6 @@ require 'json'
     end
 
 ruby_version = File.open(".ruby-version").readlines.map(&:chomp)[0]
-deps.merge!("engines" => { :ruby => "#{ruby_version}" })
+deps.merge!("engines" => { :ruby => ruby_version.to_s })
 
 File.write("gemfile.json", JSON.pretty_generate(deps)+"\n")
